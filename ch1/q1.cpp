2139625c24e214335eec822d69824be44644b4bb
Luiz Alberto <luizalbertocviana@gmail.com>
2020-06-02 03:29:33 -0300

    question 1 of chapter 1

---
+++ ch1/q1.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <set>
+#include <string>
+
+// determine if a string has all unique characters
+
+bool unique(const std::string& col){
+  std::set<char> set{};
+  for(char c : col){
+    if (set.count(c)){
+      return false;
+    }
+    else{
+      set.insert(c);
+    }
+  }
+  return true;
+}
+
+int main(int argc, char** argv){
+  if (argc == 2){
+    std::string str{argv[1]};
+
+    if (unique(str)){
+      std::cout << str << " has no repeated characters\n";
+    }
+    else{
+      std::cout << str << " has repeated characters\n";
+    }
+  }
+
+  return 0;
+}
