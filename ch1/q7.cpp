7b3fc1e659edae1d49f49d3d37d2bf69e9120652
Luiz Alberto <luizalbertocviana@gmail.com>
2020-06-15 14:21:07 -0300

    added comments

--- ch1/q7.cpp
+++ ch1/q7.cpp
@@ -3,12 +3,28 @@
 
 #include "Matrix.hpp"
 
+// given a square matrix, write a function to rotate it 90 degrees
+// clockwise in-place
+
+// this function does just that
 template<typename Type>
 bool rotate(Matrix<Type>& m){
+  // takes size_type from inside matrix
   using size_type = typename Matrix<Type>::size_type;
 
+  // if m is a square matrix, do the rotation
   if (m.num_rows == m.num_cols){
+    // first we transpose m: notice that this is equivalent to a
+    // counterclockwise rotation of 90 degrees
+
+    // as m is square, this can be done in-place
     m.transpose();
+
+    // in order to complete a clockwise rotation of 90 degrees, we
+    // take the column-wise mirror of m
+
+    // for this, we swap pairs of columns: first and left, second and
+    // second-last, and so on
 
     size_type left_col  {0};
     size_type right_col {m.num_cols - 1};
@@ -22,6 +38,7 @@ bool rotate(Matrix<Type>& m){
 
     return true;
   }
+  // otherwise just indicate that nothing was done
   else{
     return false;
   }
