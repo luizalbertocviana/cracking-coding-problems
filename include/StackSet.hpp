a6e39d5f707040313adb2f4e43d40643d8fd913d
Luiz Alberto <luizalbertocviana@gmail.com>
2020-06-22 15:40:35 -0300

    first version of StackSet.hpp

---
+++ include/StackSet.hpp
@@ -0,0 +1,62 @@
+#ifndef stackset_hpp
+#define stackset_hpp
+
+#include <array>
+#include <optional>
+#include <vector>
+
+template<typename Type, unsigned n>
+class StackSet{
+  std::vector<Type>  data_;
+  std::array<int, n> head_;
+
+  int& head_index(const unsigned i){
+    return head_[i];
+  }
+
+  Type& head_content(const unsigned i){
+    return data_[head_index(i)];
+  }
+public:
+  StackSet()
+    : data_{}, head_{}
+  {
+    data_.resize(n);
+    
+    for (unsigned i {0}; i < n; ++i){
+      head_[i] = i - n;
+    }
+  }
+
+  bool empty(unsigned i){
+    return head_index(i) < 0;
+  }
+
+  std::optional<Type> peek(const unsigned i){
+    return empty(i) ? std::nullopt : head_content(i);
+  }
+
+  void push(const unsigned i, const Type e){
+    if (head_index(i) + n >= data_.size()){
+      data_.resize(2 * data_.size());
+    }
+
+    head_index(i)   += n;
+    head_content(i)  = e;
+  }
+
+  std::optional<Type> pop(const unsigned i){
+    if (empty(i)){
+      return {};
+    }
+    else{
+      Type copy {head_content(i)};
+
+      head_index(i) -= n;
+
+      return copy;
+    }
+  }
+};
+
+#endif
