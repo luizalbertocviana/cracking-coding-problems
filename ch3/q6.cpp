11e8601236c1b2b0d3f984812417c94570a71d2a
Luiz Alberto <luizalbertocviana@gmail.com>
2020-06-28 16:07:37 -0300

    added some comments

--- ch3/q6.cpp
+++ ch3/q6.cpp
@@ -16,34 +16,49 @@
 // maintain this system and implement operations such as enqueue,
 // dequeueAny, dequeueDog and dequeueCat
 
+// a virtual class to represent an animal
 class Animal{
 public:
+  // virtual methos require some implementation; notice that derived
+  // classes can call this
   virtual std::string talk() const {return "";}
 };
 
+// one final subclass of Animal and Identity<Dog>
+
+// notice that Dog is passed as template argument to Identity; this
+// creates an id counter exclusive to Dog objects
 class Dog final : public Animal, public Identity<Dog>{
 public:
+  // default constructor
   Dog() : Animal{}, Identity{}
   {}
 
+  // copy constructor defined in terms of subclasses copy constructors
   Dog(const Dog& dog) : Animal{dog}, Identity{dog}
   {}
 
+  // copy assignment defined in terms of subclasses copy assignments
   Dog& operator=(const Dog& dog){
+    // Animal does not have a relevant copy assignment
     Identity::operator=(dog);
 
     return *this;
   }
 
+  // move constructor defined in terms of subclasses move constructors
   Dog(Dog&& dog) : Animal{std::move(dog)}, Identity{std::move(dog)}
   {}
 
+  // move assignment defined in terms of subclasses move assignments
   Dog& operator=(Dog&& dog){
+    // Animal does not have a relevant move assignment
     Identity::operator=(std::move(dog));
 
     return *this;
   }
 
+  // overrides virtual method talk of Animal
   std::string talk() const override{
     std::stringstream sstr {};
 
@@ -53,6 +68,7 @@ public:
   }
 };
 
+// this is analogous to Dog class
 class Cat final : public Animal, public Identity<Cat>{
 public:
   Cat() : Animal{}, Identity{}
@@ -85,41 +101,60 @@ public:
   }
 };
 
+// represents the shelter described in the exercise
 class Shelter{
+  // queue set of two queues, one for dogs and other for cats
   QueueSet<std::unique_ptr<Animal>, 2> queue_set_;
 
+  // we use this to identify each queue by its index
   inline static unsigned dog_queue {0};
   inline static unsigned cat_queue {1};
 
+  // template private method to put an animal into queue determined by
+  // queue_id
   template<typename Type>
   void enqueue_(Type&& animal, unsigned queue_id){
+    // pure_type represents Type with no reference mark (&)
     using pure_type = std::remove_reference_t<Type>;
 
+    // now animal is held by a unique_ptr
     auto unique_animal {std::make_unique<pure_type>(std::move(animal))};
 
+    // moves unique_animal into the queue set
     queue_set_.enqueue_at(queue_id, std::move(unique_animal));
   }
 
+  // dequeues an animal from queue indicated by queue_id
   std::unique_ptr<Animal> dequeue_type_(unsigned queue_id){
+    // we get an optional from queue set that holds the returned
+    // animal, if any
     if (auto opt {queue_set_.dequeue_from(queue_id)}){
+      // in case opt does hold a unique_ptr to an Animal, we move it
+      // to the caller
       return std::move(*opt);
     }
     else{
+      // otherwise, we return nullptr to indicate no animal was
+      // returned by queue set
       return nullptr;
     }
   }
 public:
+  // default constructor
   Shelter() : queue_set_{}
   {}
 
+  // moves dog into dog queue
   void enqueue(Dog&& dog){
     enqueue_(std::move(dog), dog_queue);
   }
 
+  // moves cat into cat queue
   void enqueue(Cat&& cat){
     enqueue_(std::move(cat), cat_queue);
   }
 
+  // takes olddest animal in shelter, if any
   std::unique_ptr<Animal> dequeue_any(){
     if (auto opt {queue_set_.dequeue()}){
       return std::move(*opt);
@@ -129,10 +164,12 @@ public:
     }
   }
 
+  // takes oldest dog, if any
   std::unique_ptr<Animal> dequeue_dog(){
     return dequeue_type_(dog_queue);
   }
 
+  // takes oldest cat, if any
   std::unique_ptr<Animal> dequeue_cat(){
     return dequeue_type_(cat_queue);
   }
