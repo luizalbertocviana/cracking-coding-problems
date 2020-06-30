eaf69548a0664bf8c3daabe7d8cc6c176e06c045
Luiz Alberto <luizalbertocviana@gmail.com>
2020-06-30 11:06:14 -0300

    uses Index to enable use of stl all_of

--- ch1/q9.cpp
+++ ch1/q9.cpp
@@ -1,5 +1,7 @@
 #include <args.hpp>
+#include <range.hpp>
 
+#include <algorithm>
 #include <iostream>
 
 // given two strings, decide whether one is rotation of the other, eg
@@ -20,16 +22,10 @@ bool rotation(const std::string& str1, const std::string& str2){
     while (check_start < str1.size()){
       // we use this to flag if str2 matches with current rotation of
       // str1
-      bool is_rotation {true};
-
-      // we iterate through str2 ...
-      for (size_type i {0}; is_rotation && i < str2.size(); i++){
-        // and compare it with the rotation of str1 starting at
-        // check_start; if one char mismatches, we flag that
-        if (str1[(check_start + i) % str1.size()] != str2[i]){
-          is_rotation = false;
-        }
-      }
+      bool is_rotation {std::all_of(index<size_type>(0), index<size_type>(str2.size()),
+                                    [&str1, &str2, check_start](const auto i) {
+                                      return str1[(check_start + i) % str1.size()] == str2[i];
+                                    })};
 
       // if str2 indeed matches current rotation of str1, then we
       // return true
